3a4e4f2909294015b4cb0373fc346bb2ecd9f22b
myangcodex <myangdb@gmail.com>
2022-02-15 23:14:34 -0800

    test with height

--- beam.js
+++ beam.js
@@ -8,7 +8,7 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
     angle: 0
   };
   
-  Cesium.knockout.track(viewModel);
+  
   
   viewer.scene.globe.depthTestAgainstTerrain = true;
   const conePosition = Cesium.Cartesian3.fromDegrees(-105.0, 40.0, 100000.0);
@@ -20,6 +20,22 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
                                Cesium.Math.toRadians(0.0),
                                Cesium.Math.toRadians(angle)));
   
+  Cesium.knockout.track(viewModel);
+  
+  //Bind the viewModel to the DOM elements of the UI that call for it.
+  const toolbar = document.getElementById("toolbar");
+  Cesium.knockout.applyBindings(viewModel, toolbar);
+  
+  
+  Cesium.knockout.getObservable(viewModel, 'angle').subscribe(
+    function(newValue) {
+      redCone.cylinder.length = newValue;
+      console.log(angle);
+    }
+  );
+  
+  
+  
   var offset = new Cesium.Cartesian3(0,0,0);
   
   var enuTransform = Cesium.Transforms.eastNorthUpToFixedFrame(conePosition);
@@ -28,7 +44,7 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
   
   Cesium.Cartesian3.add(conePosition, offset, conePosition);
   
-  const redCone = viewer.entities.add({
+  var redCone = viewer.entities.add({
     name: "Red cone",
     orientation: coneOrientation,
     position: conePosition,
@@ -39,21 +55,5 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
       material: new Cesium.Color(0,1,1,0.5),
     },
   });
-  //Bind the viewModel to the DOM elements of the UI that call for it.
-  const toolbar = document.getElementById("toolbar");
-  Cesium.knockout.applyBindings(viewModel, toolbar);
-  
-  Cesium.knockout
-    .getObservable(viewModel, "angle")
-    .subscribe(function (baseLayer) {
-      Cesium.knockout.getObservable(viewModel, 'angle').subscribe(
-          function(newValue) {
-              angle = angle;
-          }
-      );
-      var angle = viewModel.angle;
-  
-    });
   
   viewer.zoomTo(viewer.entities);
-  
